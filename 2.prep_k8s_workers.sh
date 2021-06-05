fb46528295bfd4447129735012ee1441114a2dab
Omer Abdullah Bin Mahfooz <omers.id@gmail.com>
2021-06-05 21:13:11 +0530

    Update 2.prep_k8s_workers.sh

--- 2.prep_k8s_workers.sh
+++ 2.prep_k8s_workers.sh
@@ -11,7 +11,7 @@
     curl -s http://worker1/authorized_keys > /root/.ssh/authorized_keys
  #   curl -s http://master/authorized_keys > /root/.ssh/authorized_keys
  #   scp root@master:/root/kubeadm_join_cmd.sh .
-    scp root@worker1:/root/kubeadm_join_cmd.sh .
+    scp -o "StrictHostKeyChecking no" root@worker1:/root/kubeadm_join_cmd.sh .
     chmod +x kubeadm_join_cmd.sh
     ./kubeadm_join_cmd.sh
     sleep 10
