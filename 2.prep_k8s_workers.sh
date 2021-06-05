b49fb718b8ddaad7aba8ca885565a76931c6d247
Omer Abdullah Bin Mahfooz <omers.id@gmail.com>
2021-06-05 19:02:11 +0530

    Update 2.prep_k8s_workers.sh

--- 2.prep_k8s_workers.sh
+++ 2.prep_k8s_workers.sh
@@ -1,15 +1,15 @@
 ### Preparing Worker nodes ####
-hs=`hostname`
-if [ $hs = 'master' ]
- then
-    echo "Current node is master ...  Will not be performing worker node-specific actions"
-    echo " Exiting !!!!"
-    exit
-else
+#hs=`hostname`
+#if [ $hs = 'master' ]
+# then
+#    echo "Current node is master ...  Will not be performing worker node-specific actions"
+#    echo " Exiting !!!!"
+#    exit
+# else
     echo "Current node is worker ... Performing worker node-specific actions"
     kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
     scp root@master:/root/kubeadm_join_cmd.sh .
     chmod +x kubeadm_join_cmd.sh
     ./kubeadm_join_cmd.sh
     sleep 10
-fi
+#fi
