2fd21c216562124c8120ec7ab9c0d971f594c208
Omer Abdullah Bin Mahfooz <omers.id@gmail.com>
2021-06-05 18:40:33 +0530

    Delete setup_k8_cluster.sh

--- setup_k8_cluster.sh
+++
@@ -1,7 +0,0 @@
-rm -rf ubu-k8slab
-echo "Getting config files from Github"
-git clone https://github.com/omerabm/ubu-k8slab.git
-cd ubu-k8slab
-chmod +x first_boot_k8s.sh
-echo "Preparing system for first use assiging IP address, hostnames"
-./first_boot_k8s.sh
