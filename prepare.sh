5ebed62b86902e432443e0808a467c23ae5fe119
Omer Abdullah Bin Mahfooz <omers.id@gmail.com>
2021-06-05 16:41:49 +0530

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,12 @@
 rm -rf ubu-k8slab
+echo "Preparing system for first use assiging IP address, hostname "
 git clone https://github.com/omerabm/ubu-k8slab.git
 cd ubu-k8slab
 chmod +x first_boot_k8s.sh
 ./first_boot_k8s.sh
 echo "Done .. initial setup"
-reboot
+#reboot
+#New changes to prepare the master node
+#
+echo "Preparing system for node specific changes"
+./2.prep_k8s_master.sh
