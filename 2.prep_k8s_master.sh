c71814d7a06314f1f01f60f54b565634cad298f2
Omer Abdullah Bin Mahfooz <omers.id@gmail.com>
2021-06-05 18:41:09 +0530

    Update 2.prep_k8s_master.sh

--- 2.prep_k8s_master.sh
+++ 2.prep_k8s_master.sh
@@ -2,7 +2,7 @@ hs=`hostname`
 curr_ip=$(ifconfig ens32 | grep netmask | cut -d' ' -f10)
 if [ $hs = 'master' ]
  then
-    echo "Current node is mater ... Performing master node-specific actions"
+    echo "Current node is msater ... Performing master node-specific actions"
     kubeadm init --apiserver-advertise-address=$cur_ip --pod-network-cidr=10.244.0.0/16 > ./kubeadm_init.log
     mkdir -p $HOME/.kube
     cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
