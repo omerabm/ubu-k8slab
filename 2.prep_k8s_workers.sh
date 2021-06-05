4e10d3e636b1fb78fe3492ee915d7f105c589c01
Omer Abdullah Bin Mahfooz <omers.id@gmail.com>
2021-06-05 22:50:32 +0530

    Update 2.prep_k8s_workers.sh

--- 2.prep_k8s_workers.sh
+++ 2.prep_k8s_workers.sh
@@ -6,16 +6,21 @@
 #    echo " Exiting !!!!"
 #    exit
 # else
-    echo "Current node is worker ... Performing worker node-specific actions"
+     echo "Current node is worker ... Performing worker node-specific actions"
  #   kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-    curl -s http://master/authorized_keys > /root/.ssh/authorized_keys
- #   curl -s http://master/authorized_keys > /root/.ssh/authorized_keys
- #   scp root@master:/root/kubeadm_join_cmd.sh .
- #
+     curl -s http://master/authorized_keys > /root/.ssh/authorized_keys
  #   scp -o "StrictHostKeyChecking no" root@worker1:/root/kubeadm_join_cmd.sh .
 echo "Will now start to join worker nodes to the master"
-curl -s http://master/kubeadm_join_cmd.sh > /root/ubu-k8slab/kubeadm_join_cmd.sh
+echo "Waiting to receive the kubeadm join script from master node"
+if [ -s kubeadm_join_cmd.sh ]
+ then
+    echo "Received the kubeadm join script from master node"
+    echo "============================================"
+    cat kubeadm_join_cmd.sh
+    echo "============================================"
+ else
+    curl -s http://master/kubeadm_join_cmd.sh > /root/ubu-k8slab/kubeadm_join_cmd.sh
+fi
 chmod +x /root/ubu-k8slab/kubeadm_join_cmd.sh
 ./kubeadm_join_cmd.sh
 sleep 10
-#fi
