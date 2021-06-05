2f31d4316eeca4d889eae17d3b2a6f875771a10c
Omer Abdullah Bin Mahfooz <omers.id@gmail.com>
2021-06-05 18:32:48 +0530

    Update setup_k8_cluster.sh

--- setup_k8_cluster.sh
+++ setup_k8_cluster.sh
@@ -5,5 +5,3 @@ cd ubu-k8slab
 chmod +x first_boot_k8s.sh
 echo "Preparing system for first use assiging IP address, hostnames"
 ./first_boot_k8s.sh
-echo "Done .. initial setup"
-echo "Moving to node setup for K8s services"
