11365763fdf861410bc33798791793100fe25fed
Omer Abdullah Bin Mahfooz <omers.id@gmail.com>
2021-06-05 19:51:31 +0530

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -4,7 +4,7 @@ echo "Preparing system for node specific changes"
 chmod +x 2.prep_k8s_master.sh
 chmod +x 2.prep_k8s_workers.sh
 hostname | grep -q "master" && ./2.prep_k8s_master.sh || echo "Not a master... Will skip master specific changes"
-hostname | grep -q "worker" && ./2.prep_k8s_worker.sh || echo "Not a worker .. Will skip worker specific changes"
+hostname | grep -q "worker" && ./2.prep_k8s_workers.sh || echo "Not a worker .. Will skip worker specific changes"
 echo "Applied changes"
 echo "Consider rebooting all nodes"
 sleep 15
