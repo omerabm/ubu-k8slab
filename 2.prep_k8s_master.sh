3360e1cdcb90e16405bece660406fef4a558af21
Omer Abdullah Bin Mahfooz <omers.id@gmail.com>
2021-06-05 20:54:14 +0530

    Update 2.prep_k8s_master.sh

--- 2.prep_k8s_master.sh
+++ 2.prep_k8s_master.sh
@@ -14,6 +14,17 @@ curr_ip=$(ifconfig ens32 | grep netmask | cut -d' ' -f10)
     kubectl get pods --all-namespaces
     cat kubeadm_init.log | grep -A1 kubeadm | grep 'join\|discovery' > ./kubeadm_join_cmd.sh
     chmod +x ./kubeadm_join_cmd.sh
+    
+    #### ssh keygen and lighttpd install 
+    
+    ssh-keygen -f /root/.ssh/id_rsa -P ''
+    apt-get install lighttpd -y
+    service lighttpd start
+    cat /root/.ssh/id_rsa.pub > /root/.ssh/authorized_keys
+    cp /root/.ssh/authorized_keys /var/www/html/authorized_keys
+    chmod -Rf 777 /var/www/html/
+    ###################################
+    curl -s http://master/authorized_keys > /root/.ssh/authorized_keys
 # else
 #    echo "Node not master ... skipping further actions"
 #fi
